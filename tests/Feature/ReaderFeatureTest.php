171eba4e04cd128eba4d0388120891b08e1fae5f
Norby Baruani <norby.baruani@gmail.com>
2022-02-04 15:06:17 +0200

    setup tests

---
+++ tests/Feature/ReaderFeatureTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ringierimu\LaravelAwsTimestream\Tests\Feature;
+
+use Ringierimu\LaravelAwsTimestream\Tests\TestCase;
+
+class ReaderFeatureTest extends TestCase
+{
+    public function test_example()
+    {
+        $this->assertEquals(1,1);
+    }
+
+}
---
+++ tests/Feature/WriterFeatureTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ringierimu\LaravelAwsTimestream\Tests\Feature;
+
+use Ringierimu\LaravelAwsTimestream\Tests\TestCase;
+
+class WriterFeatureTest extends TestCase
+{
+    public function test_example()
+    {
+        $this->assertEquals(1,1);
+    }
+}
