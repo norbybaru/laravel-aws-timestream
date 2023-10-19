6d3ec31a586d57732acf7519d960c4587d0fa1c7
Norby Baruani <norby.baruani@gmail.com>
2023-10-19 07:38:29 +0200

    revert builders

--- src/Builder/PayloadBuilder.php
+++ src/Builder/PayloadBuilder.php
@@ -4,21 +4,16 @@ namespace NorbyBaru\AwsTimestream\Builder;
 
 use Illuminate\Support\Carbon;
 use NorbyBaru\AwsTimestream\Contract\PayloadBuilderContract;
-use NorbyBaru\AwsTimestream\Enum\ValueTypeEnum;
 
 final class PayloadBuilder implements PayloadBuilderContract
 {
-    protected array $commonDimensions = [];
-    protected array $commonAttributes = [];
-    protected array $dimensions = [];
-    protected array $measureValues = [];
-    protected ?int $version = null;
+    protected array $dimensions;
 
     public function __construct(
         protected string $measureName,
-        protected mixed $measureValue = null,
-        protected ?Carbon $time = null,
-        protected ?ValueTypeEnum $measureValueType = null,
+        protected $measureValue,
+        protected Carbon $time,
+        protected string $measureValueType = 'DOUBLE',
         array $dimensions = []
     ) {
         if ($dimensions) {
@@ -28,57 +23,12 @@ final class PayloadBuilder implements PayloadBuilderContract
 
     public static function make(
         string $measureName,
-        mixed $measureValue = null,
-        ?Carbon $time = null,
-        ?string $measureValueType = null,
+        $measureValue,
+        Carbon $time,
+        string $measureValueType = 'DOUBLE',
         array $dimensions = []
     ): self {
-        return new self(
-            measureName: $measureName,
-            measureValue: $measureValue,
-            time: $time,
-            measureValueType: $measureValueType ? ValueTypeEnum::from($measureValueType) : null,
-            dimensions: $dimensions
-        );
-    }
-
-    public function setMeasureName(string $measureName): self
-    {
-        $this->measureName = $measureName;
-
-        return $this;
-    }
-
-    public function setMeasureValue(mixed $value): self
-    {
-        $this->measureValue = $value;
-
-        return $this;
-    }
-
-    public function setMeasureValueType(ValueTypeEnum $type): self
-    {
-        $this->measureValueType = $type;
-
-        return $this;
-    }
-
-    public function setVersion(int $version): self
-    {
-        $this->version = $version;
-
-        return $this;
-    }
-
-    public function setMultiMeasuresValues(string $name, mixed $value, ?ValueTypeEnum $type = null): self
-    {
-        $this->measureValues[] = [
-            'Name' => $name,
-            'Value' => $value,
-            'Type' => $type?->value ?? ValueTypeEnum::VARCHAR()->value,
-        ];
-
-        return $this;
+        return new self($measureName, $measureValue, $time, $measureValueType, $dimensions);
     }
 
     private function buildDimensions(string $name, $value)
@@ -87,23 +37,6 @@ final class PayloadBuilder implements PayloadBuilderContract
             'Name' => $name,
             'Value' => (string) $value,
         ];
-    }
-
-    public function setDimensions(string $name, mixed $value): self
-    {
-        $this->dimensions[] = [
-            'Name' => $name,
-            'Value' => $value,
-        ];
-
-        return $this;
-    }
-
-    public function setTime(Carbon $carbon): self
-    {
-        $this->time = $carbon;
-
-        return $this;
     }
 
     public static function buildCommonAttributes(array $attributes): array
@@ -124,44 +57,15 @@ final class PayloadBuilder implements PayloadBuilderContract
         ];
     }
 
-    private function getPreciseTime(Carbon $time): string
-    {
-        return (string) $time->getPreciseTimestamp(3);
-    }
-
-    public function getRecords(bool $batch = false): array
-    {
-        return $this->toArray($batch);
-    }
-
     public function toArray(bool $batch = false): array
     {
         $metric = [
+            'Dimensions' => $this->dimensions,
             'MeasureName' => $this->measureName,
             'MeasureValue' => (string) $this->measureValue,
+            'MeasureValueType' => $this->measureValueType,
+            'Time' => (string) $this->time->getPreciseTimestamp(3),
         ];
-
-        if ($this->measureValueType) {
-            $metric['MeasureValueType'] = $this->measureValueType->value;
-        }
-
-        if ($this->time) {
-            $metric['Time'] = $this->getPreciseTime($this->time);
-        }
-
-        if ($this->measureValues) {
-            $metric['MeasureValues'] = $this->measureValues;
-            $metric['MeasureValueType'] = 'MULTI';
-            unset($metric['MeasureValue']);
-        }
-
-        if ($this->dimensions) {
-            $metric['Dimensions'] = $this->dimensions;
-        }
-
-        if ($this->version) {
-            $metric['Version'] = $this->version;
-        }
 
         if (!$batch) {
             return [$metric];
--- src/Contract/PayloadBuilderContract.php
+++ src/Contract/PayloadBuilderContract.php
@@ -2,9 +2,11 @@
 
 namespace NorbyBaru\AwsTimestream\Contract;
 
+use Illuminate\Support\Carbon;
+
 interface PayloadBuilderContract
 {
-    public static function make(string $measureName): self;
+    public static function make(string $measureName, $measureValue, Carbon $time, string $measureValueType = 'DOUBLE', array $dimensions = []): self;
 
     public static function buildCommonAttributes(array $attributes): array;
 
